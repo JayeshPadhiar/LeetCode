1abd7557517c3654314121a387cb30fd075377b4
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2021-09-18 15:55:30 +0530

    Time: 729 ms (26.40%), Space: 180.2 MB (56.30%) - LeetHub

--- swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -11,73 +11,26 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        
-        if(!head->next)
-            return head;
-        
-        else if(!head->next->next)
-        {            
-            head->next->next = head;
-            head = head->next;
-            head->next->next = NULL;
-            return head;
-        }
-        
-        else if(k==1)
-        {
-            ListNode *trav = head;
-            while(trav->next->next)
-                trav = trav->next;
-            trav->next->next = head->next;
-            ListNode *newhead = trav->next;
-            trav->next = head;
-            head->next = NULL;
-            return newhead;            
-        }
-        
-        else {
-            
-            int frombegin = k-2;
-            ListNode *trav = head;
-            
-            while(frombegin)
-            {
-                trav = trav->next;
-                frombegin--;
-            }
-            
-            if(!trav->next->next)
-            {
-                int temp = trav->next->val;
-                trav->next->val = head->val;
-                head->val = temp;
-                
-                return head;
-            }
-            
-            ListNode *a = trav;
-            ListNode *b = head;
-            
-            trav = trav->next;
-            
-            while(trav->next->next)
-            {
-                trav = trav->next;
-                b = b->next;
-                
-            }
-            
-            if(a == b)
-            {
-                return head;
-            }
-            
-            int temp = a->next->val;
-            a->next->val = b->next->val;
-            b->next->val = temp;
     
-            return head;
-        }
+    if(head==nullptr || head->next==nullptr)
+    return head;
+    
+    ListNode *slow=head,*fast=head;
+    
+    int i=k-1;
+    while(i-- && fast->next!=nullptr)
+    {
+        fast=fast->next;
+    }
+    ListNode *temp=fast;
+    while(fast!=nullptr && fast->next!=nullptr)
+    {
+         slow=slow->next;
+        fast=fast->next;
+    }
+    swap(temp->val,slow->val);
+    return head;
+
         
     }
 };
