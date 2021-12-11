9871b4424dbc8b866b74ae036d3d5374eef8e991
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2021-12-11 21:30:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.83%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1)
-            return n;
-        
-        return fib(n-1)+fib(n-2);
+        int x=0, y=1, ans=y;
+	
+	    if(n == 0)
+		    return 0;
+
+        for(int i=1; i<n; i++)
+        {
+            cout<<ans<<endl;
+            ans = x+y;
+            x = y;
+            y = ans;
+        }
+
+        return ans;
+
         
     }
 };
