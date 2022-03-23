64172d87e89ddf65a1d3072cd1bb76ac43526be8
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-03-23 12:59:24 +0530

    Time: 12 ms (15.97%), Space: 9.9 MB (83.12%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -5,8 +5,8 @@ public:
         
         for(int i=0; i<prices.size(); i++){
             for(int j=i+1; j<prices.size(); j++){
-                if(prices[j] <= prices[i]){
-                    prices[i] -= prices[j];
+                if(prices.at(j) <= prices.at(i)){
+                    prices[i] -= prices.at(j);
                     break;
                 }
                 
