ab4c6c7a5d5c0a4dd0e4ab47c172d70b16f00b58
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-03-28 21:03:38 +0530

    Time: 2 ms (46.75%), Space: 63.8 MB (75.65%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -8,20 +8,24 @@ class Solution {
         int r = nums.length-1;
         int l = nums.length-1-x;
         
-        Stack<Integer> s = new Stack();
+        
+        
+        int[] sub = new int[x];
         
         int i = x;
         
         while(i-- != 0){
-            s.push(nums[nums.length-1-i]);
+            sub[i] = (nums[nums.length-1-i]);
         }
         
         while(l >= 0){
             nums[r--] = nums[l--];
         }
         
+        i=0;
         while(r >= 0){
-            nums[r--] = s.pop();
+            nums[r] = sub[i++];
+            r--;
         }
        
     }
