a7fbb7f406d2717f9153e2bc491ab9fe57a5eb77
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-03-28 20:25:18 +0530

    Time: 2 ms (80.17%), Space: 55.3 MB (66.67%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,49 +1,20 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         
-        Stack<Integer> x = new Stack();
-        
-        int pos = 0;
-        
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]<0)
-                x.push(nums[i] * nums[i]);
-            else{
-                pos = i;
-                break;
-            }
-        }
-        
         int[] ans = new int[nums.length];
+        int l = 0;
+        int r = nums.length-1;
         
-        for(int i=0; i<nums.length; i++){
+        for(int i=nums.length-1; i>=0; i--){
             
-            
-            if(pos < nums.length){
-                int xpos = nums[pos]*nums[pos];
-                if(!x.empty()){   
-                    if(pos < nums.length && xpos < x.peek()){
-                        ans[i] = xpos;
-                        pos++;
-                    }else{
-                        ans[i] = x.pop();
-                    }
-                }
-                else{
-                    ans[i] = xpos;
-                    pos++;
-                }
+            if(Math.abs(nums[r]) > Math.abs(nums[l])){
+                ans[i] = nums[r]*nums[r--];
+            }else{
+                ans[i] = nums[l]*nums[l++];
             }
-            else{ 
-                if(!x.empty()){    
-                    ans[i] = x.pop();
-                }
-            }
-            
         }
         
         return ans;
-        
         
         
     }
