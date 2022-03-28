3395a676b48e95c1e8f29c43c10201a1603ea81f
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-03-28 15:12:53 +0530

    Time: 10 ms (90.57%), Space: 45.6 MB (45.97%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         
         while(s.contains("abc")){
-            s = s.replaceAll("abc", "");
+            s = s.replace("abc", "");
         }
         
         if(s == "")
