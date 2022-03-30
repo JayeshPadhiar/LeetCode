65c195b7fb90670e3a54533843cb51c2317c903c
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-03-30 09:50:58 +0530

    Time: 30 ms (9.73%), Space: 41.6 MB (31.78%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int i) {
+        
+        long x=0;
+        
+        
+        while(x*x <= i){
+            x++;
+        }
+        
+        return (int)x-1;
+    }
+}
