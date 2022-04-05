d72610f847f310477bbb553de77f03527b92e92f
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-04-05 14:28:45 +0530

    Time: 8 ms (28.51%), Space: 43 MB (13.12%) - LeetHub

---
+++ 27-remove-element/27-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int count = 0;
+        
+        for(int right=0; right<nums.length; right++){
+            if(nums[right] != val){
+                System.out.println(nums[right]);
+                nums[left++] = nums[right];
+                continue;
+            }
+            count++;
+        }
+        return nums.length-count;
+    }
+}
