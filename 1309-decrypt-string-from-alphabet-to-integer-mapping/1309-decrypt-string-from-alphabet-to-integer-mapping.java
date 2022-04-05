ba15d03de78bf951e607de804701bdc9fd344c24
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-04-05 10:33:48 +0530

    Time: 9 ms (19.50%), Space: 42.9 MB (19.77%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -6,22 +6,17 @@ class Solution {
         
         int i = str.length-1;
         for(i=str.length-1; i>=2; i--){
-            
             if(str[i] == '#'){
                 char x = (char)((int)((str[i-2]=='1')? 'j': 't') + ((int)(str[i-1]) - 48));
                 ans = x+ans;
                 i=i-2;
-                System.out.println(i);
-                
             }else{
                 ans = (char)(str[i]+48) + ans;
-                System.out.println((char)(str[i]+48));
             }
         }
         
         while(i >=0 ){
-            ans = (char)(str[i]+48) + ans;
-            System.out.println((char)(str[i--]+48));
+            ans = (char)(str[i--]+48) + ans;
         }
         
         return ans;
