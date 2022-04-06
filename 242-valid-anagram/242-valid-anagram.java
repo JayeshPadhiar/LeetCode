39b6fe2d73d2f0f7701623a7a6c034422ad8aeb2
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-04-06 14:57:28 +0530

    Time: 67 ms (5.07%), Space: 51.3 MB (5.54%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -11,7 +11,6 @@ class Solution {
         
         int i=0;
         while(i<a.length){
-            //System.out.println(a[i] + " " + b[i]);
             if(!a[i].equals(b[i])){
                 return false;
             }
