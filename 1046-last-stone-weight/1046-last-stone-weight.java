af2fb8b7e942f778c2c90b2ca5c0534297fe253a
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-04-07 13:07:14 +0530

    Time: 1 ms (98.65%), Space: 41.7 MB (47.75%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,34 +1,50 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         
-        
-        int a = stones.length-1;
-        int b = stones.length-2;
+        int a=0, b=0;
         
         if(stones.length < 2)
             return stones[0];
         
-        Arrays.sort(stones);
-        while(stones[b] != 0){
-            for(int i: stones)
-                System.out.print(i);
+        while(true){
+            int fir = Integer.MIN_VALUE;
+            int sec = Integer.MIN_VALUE;
+            
+            for(int i=0; i<stones.length; i++){
+                
+                if(stones[i] >= fir){
+                    sec = fir;
+                    b = a;
+                    fir = stones[i];
+                    a = i;
+                }
+                else if(stones[i] > sec){
+                    sec = stones[i];
+                    b = i;
+                }
+            }
+            
+            //System.out.println(stones[a] + " " + stones[b]);
             
             if(stones[a]==0 && stones[b]==0)
                 return 0;
             
+            if(stones[a]==0 || stones[b]==0)
+                break;
+            
             if(stones[a] == stones[b]){
-                stones[a]=0;
-                stones[b]=0;
+                stones[a] = 0;
+                stones[b] = 0;
             }else if(stones[a] <= stones[b]){
-                stones[b]-=stones[a];
-                stones[a]=0;
+                stones[b] -= stones[a];
+                stones[a] = 0;
             }else{
-                stones[a]-=stones[b];
-                stones[b]=0;
+                stones[a] -= stones[b];
+                stones[b] = 0;
             }
-            Arrays.sort(stones);
         }
         
-        return stones[a];
+        return (stones[a]==0)?stones[b]:stones[a];
+        
     }
 }
