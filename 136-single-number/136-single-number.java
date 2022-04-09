89ca16534176dab9f71ad7141e7d2428c20778f7
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-04-09 11:30:36 +0530

    Time: 2 ms (54.37%), Space: 50.1 MB (75.31%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,22 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Boolean> map = new HashMap();
-        
-        
-        for(int i: nums){
-            if(map.containsKey(i)){
-                map.remove(i);
-            }
-            
-            else{
-                map.put(i, true);
-            }
+        for(int i=0; i<nums.length-1; i++){
+            nums[i+1] = nums[i]^nums[i+1];
         }
-        
-        for(int i: map.keySet())
-            return i;
-        
-        return 0;
-        
+        return nums[nums.length-1];
     }
 }
