dfbd9953a0d98c4e58ea3fc50f2d24086bc64875
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-06-05 15:02:56 +0530

    Time: 19 ms (65.08%), Space: 10.4 MB (48.25%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -4,11 +4,24 @@ public:
         
         int ans=0;
         
-        for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] < 0)
-                    ans++;
-            }
+        for(int i=0; i<grid.size(); i++){             
+                
+                int left=0;
+                int right = grid[0].size()-1;
+                int mid=0;
+                
+                while(right >= left){
+                    mid = left+(right-left)/2;
+                    
+                    if(grid[i][mid] >= 0){
+                        left = mid+1;
+                    }
+                    else{
+                        right = mid-1;
+                    }
+                }
+                
+                ans+=grid[0].size()-right-1;
         }
         
         return ans;
