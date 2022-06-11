b4a8e3181c0ade4ebae1d5f97d55945173eb9449
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-06-11 13:20:35 +0530

    Time: 1131 ms (5.09%), Space: 177.3 MB (6.69%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        
+        vector<int> a;
+        unordered_map<int, int> b;
+        int steps=-1, i=0;
+        
+        int sum=0;
+        while(i<nums.size()){
+            sum+=nums[i];
+            
+            if(sum == x){
+                cout<<"A"<<endl;
+                steps = i+1;
+            }
+            
+            a.push_back(sum);
+            i++;
+        }
+        
+        if(sum < x){
+            return -1;
+        }
+        
+        sum=0;
+        i=nums.size()-1;
+        while(i>=0){
+            sum+=nums[i];
+            
+            if(sum == x){
+                
+                if(steps == -1){
+                    steps = nums.size()-i;
+                }
+                
+                if(steps!=-1 && steps>nums.size()-i){
+                    steps = nums.size()-i;
+                }
+                
+                cout<<"B"<<endl;
+            }
+            
+            b[sum] = nums.size()-i;
+            i--;
+        }
+        
+        cout<<"Steps: "<<steps<<endl;
+        
+        
+        for(int i=0; i<a.size(); i++){
+            if(b.find(x-a[i]) != b.end()){
+                if(i+b[x-a[i]] <= nums.size()){
+                    cout<<"yo";
+                    if(steps == -1){
+                        steps = i+b[x-a[i]]+1;
+                    }else if(steps > i+b[x-a[i]]){
+                        steps = i+b[x-a[i]]+1;
+                    }
+                }
+            }
+        }
+        
+        return steps;
+    }
+};
