696698938daeb95794944db54c939402d79a29f6
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2022-06-13 11:59:39 +0530

    Time: 3 ms (97.83%), Space: 8.5 MB (91.19%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=tri.size()-1; i>=1; i--){
             for(int j=0; j<tri[i].size()-1; j++){    
                 tri[i-1][j] += min(tri[i][j], tri[i][j+1]);
-                cout<<min(tri[i][j], tri[i][j+1])<<" ";
             }
         }
         
