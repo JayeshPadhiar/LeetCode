fb1a001722bad0d7495e044e5f0f78ff541fa15c
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2025-01-06 15:25:49 +0530

    Time: 78 ms (10.44%), Space: 56.8 MB (5.21%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fast = head;    
+    let slow = head;
+
+    if(!head || !head.next){
+        return false;
+    }
+    
+    slow = slow.next;
+    fast = fast.next.next;
+
+    while(fast && fast.next){
+        if(fast == slow){
+            return true;
+        }
+
+        slow = slow.next;
+        fast = fast.next.next;
+    }    
+
+    return false;
+};
