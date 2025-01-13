e73b7dc093b37461f0a5950cb4a67a1f528feec1
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2025-01-13 16:20:15 +0530

    Time: 12 ms (41.79%), Space: 81.5 MB (56.72%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
@@ -20,8 +20,14 @@ var nodesBetweenCriticalPoints = function(head) {
     let index = 2;
     let indexes = [];
 
+    let min = 100000;
+
     while(last){
         if((mid.val > first.val && mid.val > last.val) || (mid.val < first.val && mid.val < last.val)){
+            let diff = index - indexes[indexes.length-1];
+            if(indexes.length && min >= diff){
+                min = diff;
+            }
             indexes.push(index);
         }
 
@@ -35,18 +41,6 @@ var nodesBetweenCriticalPoints = function(head) {
         return [-1,-1];
     }else{
         let len = indexes.length-1;
-        let min = indexes[1]-indexes[0];
-
-        let i = 0;
-
-        while(i+1 <= len){
-            let diff = indexes[i+1] - indexes[i]
-            if(diff < min){
-                min = diff;
-            }
-            i++;
-        }
-
         let max = indexes[len] - indexes[0];
         return [min, max];
     }
