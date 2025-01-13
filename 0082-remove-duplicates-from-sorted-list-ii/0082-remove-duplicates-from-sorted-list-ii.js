a5e7c1849559aef71154ab1b3cf4096a940bb271
Jayesh Padhiar <jayeshpadhiar20@gmail.com>
2025-01-13 13:59:52 +0530

    Time: 1 ms (59.3%), Space: 51.3 MB (49.28%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -16,29 +16,15 @@ var deleteDuplicates = function(head) {
     let y = head;
 
     while(y){
-        let isDuplicate = false;
-
-        while(y.next && y.next.val == y.val){
-            isDuplicate = true;
-            y = y.next;
+        if(y.next && y.next.val == y.val){
+            while(y.next && y.next.val == y.val){
+                y = y.next;
+            }
+            x.next = y.next;
+        }else{
+            x = x.next;
         }
-
-        if(isDuplicate){
-            y = y.next;
-        }
-
-        x.next = y;
-
-        if(y && y.next && y.val == y.next.val){
-            continue;
-        }
-        
-        x = x.next;
-
-        if(y){
-            y = y.next;
-        }
-        
+        y = y.next;
     }
 
     return ans.next;
